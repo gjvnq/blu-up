033f3f2f4ad535fdd998e5931a7d2f1ecdd0d8b4
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-03-28 21:24:25 -0300

    Basic volume manipulation working.

--- main.go
+++ main.go
@@ -1,5 +1,91 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/logrusorgru/aurora"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/cobra"
+)
+
+var FlagUUID string
+var DBPath string
+var DB *sql.DB
+
+const VERSION = "v0.0.1"
+
+var rootCmd = &cobra.Command{
+	Use:   "blu-up [command]",
+	Short: "blu-up a simple backup tool",
+	Long:  "A hash based backup tool capable of multiple volumes, links and deduplication. https://github.com/gjvnq/blu-up",
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of blu-up",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(VERSION)
+	},
+}
+
+var runCmd = &cobra.Command{
+	Use:   "run [db path] [folder to backup] [volume name] [volume path]",
+	Short: "Backups a folder",
+	Args:  cobra.ExactArgs(3),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v0.0.1")
+	},
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init [db path]",
+	Short: "Starts an empty backup database",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// Load DB
+		LoadDB(args)
+		defer DB.Close()
+		// Run commands
+		_, err := DB.Exec(CREATE_DB_SQL)
+		CheckEnd(err)
+	},
+}
+
+func LoadDB(args []string) {
+	var err error
+	DBPath, err = filepath.Abs(args[0])
+	CheckEnd(err)
+	if !strings.HasSuffix(DBPath, ".sqlite") {
+		CheckEnd("db path must end with .sqlite")
+	}
+	DB, err = sql.Open("sqlite3", DBPath)
+	CheckEnd(err)
+	// fmt.Println("Opened", DBPath)
+}
+
+func CheckEnd(err interface{}) {
+	if err == nil {
+		return
+	}
+	fmt.Println(aurora.Bold(aurora.Red(err)))
+	os.Exit(1)
+}
+
+func main() {
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(initCmd)
+	volAddCmd.Flags().StringVarP(&FlagUUID, "uuid", "", "", "Force specific UUID for new volume instead of generating a new one")
+	volCmd.AddCommand(volAddCmd)
+	volCmd.AddCommand(volRmCmd)
+	volCmd.AddCommand(volLsCmd)
+	rootCmd.AddCommand(volCmd)
+	rootCmd.AddCommand(runCmd)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- volume.go
+++ volume.go
@@ -1,7 +1,91 @@
 package main
 
+import (
+	"fmt"
+
+	uuid "github.com/gjvnq/go.uuid"
+	"github.com/logrusorgru/aurora"
+	"github.com/spf13/cobra"
+)
+
 type Vol struct {
 	UUID string `json:uuid`
 	Name string `json:name`
 	Desc string `json:desc`
+}
+
+func NewVol() Vol {
+	vol := Vol{}
+	vol.UUID = uuid.NewV4().String()
+	return vol
+}
+
+var volCmd = &cobra.Command{
+	Use:   "vol",
+	Short: "Manage volumes",
+}
+
+var volAddCmd = &cobra.Command{
+	Use:   "add [db path] [name] [desc]",
+	Short: "Adds a volume to the database",
+	Args:  cobra.RangeArgs(2, 3),
+	Run: func(cmd *cobra.Command, args []string) {
+		// Load DB
+		LoadDB(args)
+		defer DB.Close()
+		// Query
+		vol := NewVol()
+		if FlagUUID != "" {
+			vol.UUID = FlagUUID
+		}
+		vol.Name = args[1]
+		if len(args) > 2 {
+			vol.Desc = args[2]
+		}
+		_, err := DB.Exec("INSERT INTO `volumes` (`uuid`, `name`, `desc`) VALUES (?, ?, ?);", vol.UUID, vol.Name, vol.Desc)
+		CheckEnd(err)
+	},
+}
+
+var volRmCmd = &cobra.Command{
+	Use:   "rm [db path] [uuid]",
+	Short: "Removes a volume from the database",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		// Load DB
+		LoadDB(args)
+		defer DB.Close()
+		// Query
+		_, err := DB.Exec("DELETE FROM `volumes` WHERE `uuid` = ?", args[1])
+		CheckEnd(err)
+	},
+}
+
+var volLsCmd = &cobra.Command{
+	Use:   "ls [db path]",
+	Short: "Lists the volumes in the database",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		flag_empty := true
+
+		// Load DB
+		LoadDB(args)
+		defer DB.Close()
+		// Query
+		rows, err := DB.Query("SELECT `uuid`, `name`, `desc` FROM `volumes`;")
+		CheckEnd(err)
+		defer rows.Close()
+		for rows.Next() {
+			flag_empty = false
+			var uuid string
+			var name string
+			var desc string
+			err := rows.Scan(&uuid, &name, &desc)
+			CheckEnd(err)
+			fmt.Println(uuid, aurora.Bold(name), desc)
+		}
+		if flag_empty {
+			fmt.Println("no volumes in the database")
+		}
+	},
 }
